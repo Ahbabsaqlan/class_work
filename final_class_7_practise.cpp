2e4ba5f2cdbb342701b79b5f016d1265947b3c51
Ahbabsaqlan <ahbabzami3@gmail.com>
2022-08-02 10:17:05 +0600

    OBP 8 class practise 1

--- final_class_7_practise.cpp
+++ final_class_7_practise.cpp
@@ -1,8 +1,7 @@
 #include<iostream>
 using namespace std;
 class building{
-    int per_sqr_cost;
-    int total_sqr_feet;
+    int per_sqr_cost,total_sqr_feet;
     public:
     building(int cost,int total_feet){
         per_sqr_cost=cost;
@@ -13,15 +12,30 @@ class building{
         return total_cost;
     }
 };
+class residence : public building{
+    int cost_per_elec_eqpmnt,number_eqpmnt;
+    public:
+    residence(int cost_per,int num_eqpmnt,int b,int c):building(b,c){
+        cost_per_elec_eqpmnt=cost_per;
+        num_eqpmnt=num_eqpmnt;
+    }
+    int total_cost(){
+        int total_cost=cost_per_elec_eqpmnt*number_eqpmnt;
+        return total_cost;
+    }
+};
+class dormitory :protected residence{
+    protected:
+    int num_residence;
+    public:
+    dormitory(int x,int p,int q,int r,int s):residence(p,q,r,s){
+        num_residence=x;
+    }
+};
 
 int main(){
-    int cost_feet,feet;
-    cin>>cost_feet>>feet;
-    building building(cost_feet,feet);
-    int cost=building.building_cost();
-    cout<<cost;
     
-
-
+    dormitory D(5000,5,10,15,20);
+    
  return 0;
 }
